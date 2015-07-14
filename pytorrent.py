5601a9b1159c58cea91756f9a376c3b734073c1a
John Wyant <john.wyant@gmail.com>
2015-07-13 23:28:08 -0400

    Get rid of fancy dir name gobbledegook
    Wasn’t working as expected, so went back to simple stuff.

--- pytorrent.py
+++ pytorrent.py
@@ -25,8 +25,6 @@ def to_unicode(text):
 
 metainfo = bencode.bdecode(open(os.path.expanduser(''.join(sys.argv[2:])),'rb').read())
 
-#print metainfo
-
 if sys.argv[1] == 'tracker':
     sys.stdout.write(urlparse.urlparse(metainfo['announce']).hostname)
     sys.exit(0)
@@ -39,29 +37,6 @@ elif sys.argv[1] == 'base_directory':
 elif sys.argv[1] == 'initial_infohash_directory':
     sys.stdout.write(os.path.join(os.path.expanduser('~/rtorrent/torrents'),hashlib.sha1(bencode.bencode(metainfo['info'])).hexdigest().upper()))
     sys.exit(0)
-elif sys.argv[1] == 'complete_directory':
-    name = metainfo['info']['name']
-    try:
-    	tempfiles = metainfo['info']['files']
-    	files = []
-    	size = 0
-    	for tf in tempfiles:
-    		files.append({'filename':'/'.join(tf['path']),'filesize':tf['length']})
-    		size += tf['length']
-    except KeyError as e:
-    	files = [{'filename':metainfo['info']['name'],'filesize':metainfo['info']['length']}]
-    	size = metainfo['info']['length']
-
-    numfiles = 0
-    for f in files:
-    	numfiles += 1
-
-
-    if numfiles > 1:
-    	torrentfolder = to_unicode(name)+'/'
-    elif numfiles == 1 and files[0]['filename'] != name:
-    	torrentfolder = to_unicode(name)+'/'
-    else:
-    	torrentfolder = ''
-    sys.stdout.write(os.path.join(os.path.expanduser('~/rtorrent/completed'),hashlib.sha1(bencode.bencode(metainfo['info'])).hexdigest().upper(),torrentfolder))
+elif sys.argv[1] == 'completed_infohash_directory':
+    sys.stdout.write(os.path.join(os.path.expanduser('~/rtorrent/completed'),hashlib.sha1(bencode.bencode(metainfo['info'])).hexdigest().upper()))
     sys.exit(0)
